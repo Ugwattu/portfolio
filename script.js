41c74c516081da31560c5def56e7c50a47d70658
Ugwattu <umair.ghafoor06@gmail.com>
2025-05-23 20:14:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
-    const mailtoLink = `mailto:your@email.com?subject=Contact Form Message from ${encodeURIComponent(name)}&body=${encodeURIComponent(message)}%0A%0AFrom: ${encodeURIComponent(email)}`;
+    const mailtoLink = `mailto:umair.ghafoor06@gmail.com?subject=Contact Form Message from ${encodeURIComponent(name)}&body=${encodeURIComponent(message)}%0A%0AFrom: ${encodeURIComponent(email)}`;
     
     window.location.href = mailtoLink;
   });
