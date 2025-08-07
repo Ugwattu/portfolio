2f6070fa7ed5ee19ab0eb4b5839fbd951a1fa61c
Ugwattu <umair.ghafoor06@gmail.com>
2025-08-07 19:18:27 +0500

    Lazy Loading

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = mailtoLink;
   });
 });
+
+//Lazy Loading
+document.addEventListener("DOMContentLoaded", function() {
+      const images = document.querySelectorAll('img');
+      images.forEach(img => {
+        img.setAttribute('loading', 'lazy');
+      });
+    });
