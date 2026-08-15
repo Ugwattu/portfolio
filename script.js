a7138aabfb807b274423e4e12b8b9135867db82e
Ugwattu <ugwattu@example.com>
2026-08-15 10:46:01 +0500

    added pfsense(may replace with OPNsense)

--- script.js
+++ script.js
@@ -104,7 +104,8 @@ var tools = [
   { name: "Syncthing", url: "https://syncthing.net/", icon: "https://avatars.githubusercontent.com/u/7628018?s=200&v=4", cat: "prod" },
   { name: "Frigate", url: "https://frigate.video/", icon: "https://raw.githubusercontent.com/blakeblackshear/frigate/a573ea49bffb0a8a6dcd4cc83db01aaf409bd4e6/docs/static/img/branding/logo.svg", cat: "sec" },
   { name: "Ladybird", url: "https://ladybird.org", icon: "https://raw.githubusercontent.com/LadybirdBrowser/ladybird/refs/heads/master/Base/res/icons/128x128/app-browser.png", cat: "sec" },
-  { name: "OpenVPN", url: "https://openvpn.net/", icon: "https://avatars.githubusercontent.com/u/1569141?s=200&v=4", cat: "sec" }
+  { name: "OpenVPN", url: "https://openvpn.net/", icon: "https://avatars.githubusercontent.com/u/1569141?s=200&v=4", cat: "sec" },
+  { name: "pfSense", url: "https://www.pfsense.org/", icon: "https://avatars.githubusercontent.com/u/3716965?s=200&v=4", cat: "sec" }
 ];
 
 var toolsGrid = document.getElementById('toolsGrid');
